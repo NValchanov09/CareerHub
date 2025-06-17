d1a54306b06095fd52fd82524321810577c9f394
Nikolay Valchanov <nikolay.valchanov09@gmail.com>
2025-06-17 17:29:10 +0300

    Added Language Model

---
+++ Models/Language.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CareerHub.Models
+{
+    public class Language
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? LogoPath { get; set; }
+
+        public ICollection<JobPosition> JobPositions { get; set; } = new List<JobPosition>();
+    }
+}
