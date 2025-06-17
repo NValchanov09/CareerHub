450d10a549654c8983c6858c865082e69b85ad36
Nikolay Valchanov <nikolay.valchanov09@gmail.com>
2025-06-17 17:28:47 +0300

    Added ApplicationUser Model (as a inheritance from IdentityUser)

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace CareerHub.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Description { get; set; }
+
+        public string? LinkedInURL { get; set; }
+
+        public string? GitHubURL { get; set; }
+
+        public string? ContactEmail { get; set; }
+
+        public DateTime RegisteredAt { get; set; } = DateTime.Now;
+
+        [ForeignKey("Company")]
+        public int? CompanyId { get; set; }
+
+        public Company? Company { get; set; }
+    }
+}
