f8c075bf667c6fa09842ff0b8a9becba2ae61f8f
Nikolay Valchanov <nikolay.valchanov09@gmail.com>
2025-06-20 18:20:22 +0300

    Made the javascript for the checkboxes multi-use (can be used with only injection without copy-pasting)

--- wwwroot/js/crud_table.js
+++ wwwroot/js/crud_table.js
@@ -18,3 +18,73 @@
         });
     });
 });
+
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+
+    var checkbox = $('table tbody input[type="checkbox"]');
+    $("#selectAll").click(function () {
+        if (this.checked) {
+            checkbox.each(function () {
+                this.checked = true;
+            });
+        } else {
+            checkbox.each(function () {
+                this.checked = false;
+            });
+        }
+    });
+    checkbox.click(function () {
+        if (!this.checked) {
+            $("#selectAll").prop("checked", false);
+        }
+    });
+
+    $("#deleteSelected").click(function () {
+        var selectedItems = [];
+
+        $('table tbody input[name="checkbox"]:checked').each(function () {
+            selectedItems.push($(this).val());
+        });
+
+        var deleteUrl = $(this).data('delete-url');
+
+        $('.cancelDelete').click(function () {
+            $('#confirmModal').modal('hide');
+        });
+
+        $('#confirmModal').on('click', function (e) {
+            if ($(e.target).hasClass('modal')) {
+                $(this).modal('hide');
+            }
+        });
+
+        if (selectedItems.length > 0) {
+            $('#confirmModal').modal('show');
+
+            $('#confirmDelete').off('click').on('click', function () {
+                $.ajax({
+                    url: deleteUrl,
+                    type: 'POST',
+                    contentType: 'application/json; charset=utf-8',
+                    data: JSON.stringify(selectedItems),
+                    headers: {
+                        'RequestVerificationToken': $('input[name="__RequestVerificationToken"]').val()
+                    },
+                    success: function (response) {
+                        if (response.success) {
+                            window.location.reload();
+                        } else {
+                            alert(response.message || "Error deleting languages.");
+                        }
+                    },
+                    error: function (xhr, status, error) {
+                        alert("An error occurred: " + error);
+                    }
+                });
+
+                $('#confirmModal').modal('hide');
+            });
+        }
+    });
+});
